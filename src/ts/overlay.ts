08cf6fadb6fa15e8c61ba44e6240c96cab6f893c
reynevan24 <szymon.baranczyk@gmail.com>
2023-04-22 23:02:51 +0200

    added additional options to overlay

--- src/ts/overlay.ts
+++ src/ts/overlay.ts
@@ -6,6 +6,9 @@ export type OverlayConfig = {
   positionY?: string;
   offsetX?: string;
   offsetY?: string;
+  fadeOnClose?: boolean;
+  closeTime?: number;
+  closeAllWindows?: boolean;
 }
 
 type NormalizedOverlayConfig = Required<OverlayConfig>;
@@ -20,20 +23,48 @@ export const setupOverlaySocket = (socket: ModuleSocket) => {
   socket.register('createOverlay', handleOverlayCreation);
 }
 
+
+
 const handleOverlayCreation = async (config: OverlayConfig, html: string) => {
   const normalizedConfig = normalizeConfig(config);
   let template = await renderTemplate(`modules/${moduleId}/templates/overlay.hbs`, normalizedConfig);
   const overlay = $(template).get()[0];
   overlay.innerHTML = html;
   document.body.append(overlay);
+
+  if(config.closeAllWindows) {
+    closeAllWindows();
+  }
+
+  if(normalizedConfig.closeTime > 0) {
+    await handleClosingOverlay(overlay, normalizedConfig);
+  }
+
   return overlay;
+};
+const handleClosingOverlay = async (overlay: HTMLElement, config: NormalizedOverlayConfig) => {
+  await sleeper(config.closeTime * 1000);
+  if(config.fadeOnClose) {
+    overlay.classList.add('fade-out');
+    await sleeper(2000);
+  }
+  overlay.remove();
 };
 
 const normalizeConfig = (config: OverlayConfig): NormalizedOverlayConfig => {
   return {
-    positionX: config.positionX || 'start',
-    positionY: config.positionY || 'start',
+    positionX: config.positionX || 'center',
+    positionY: config.positionY || 'center',
     offsetX: config.offsetX || '0px',
-    offsetY: config.offsetY || '0px'
+    offsetY: config.offsetY || '0px',
+    fadeOnClose: config.fadeOnClose || true,
+    closeTime: config.closeTime || 15,
+    closeAllWindows: config.closeAllWindows || true
   }
 }
+
+const sleeper = (time: number) => new Promise(resolve => setTimeout(resolve, time))
+
+const closeAllWindows = () => {
+  document.querySelectorAll('.header-button.close').forEach(button => (button as HTMLElement).click());
+};
