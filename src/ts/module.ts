1bcff08e5902aa9a98c63af2c4fb6cc1268d161b
reynevan24 <szymon.baranczyk@gmail.com>
2023-04-22 22:37:26 +0200

    fix game modules access

--- src/ts/module.ts
+++ src/ts/module.ts
@@ -1,20 +1,14 @@
 // Do not remove this import. If you do Vite will think your styles are dead
 // code and not include them in the build output.
 import '../styles/overlay.scss';
-import { moduleId } from './constants';
 import { AnarchistOverlayModule } from './types';
 import { setupSocket } from './socket';
 import { createOverlay, setupOverlaySocket } from './overlay';
-
-let module: AnarchistOverlayModule;
-
-Hooks.once('init', () => {
-  console.log(`Initializing ${moduleId}`);
-
-  module = (game as Game).modules.get(moduleId) as AnarchistOverlayModule;
-});
+import { moduleId } from "./constants";
 
 Hooks.once('socketlib.ready', () => {
+  const module = (game as Game).modules.get(moduleId) as AnarchistOverlayModule;
+
   const socket = setupSocket();
   setupOverlaySocket(socket);
   module.createOverlay = createOverlay(socket);
